e792e686669c33b4f86149431ae275b651c5e017
hasanur-rahman079 <hasanurrahman.bge@gmail.com>
2023-05-24 20:20:59 +0600

    updated home info

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,17 +5,17 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm Hasanur. I'm the Graduate Research Assistant at BAU where i am
-      doing research on bioinformatics.
+      Hey, I'm Hasanur, a Graduate Research Assistant at BAU, where researching
+      on medicinal plants and their drug therapeutics in diseases.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      I'm a fresh biotechnology and genetic engineering graduate interested in
-      cancer informatics, and molecular drug designing and development.
-      Currently preparing for MSc/PhD studies in abroad.
+      I'm a Biotechnology and Genetic Engineering graduate interested in cancer
+      informatics, molecular drug designing and development. Currently
+      seeking for PhD position in abroad.
     </>
   );
 };
